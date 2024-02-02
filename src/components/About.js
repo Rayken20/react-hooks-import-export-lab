6dd5325aeb1f72a8e44e0fcb8ea1c43e8daf024a
Rachael <rachael92njoki@gmail.com>
2024-02-02 19:58:49 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import {image} from "../data/user";
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
+      <h2>About me</h2>
       <p>I Made This</p>
       <img src={image} alt="I made this" />
     </div>
